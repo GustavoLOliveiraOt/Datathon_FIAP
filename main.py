f08ce96f2f9a069b4d7563ec09280a5830d5dce0
GustavoLOliveiraOt <136750644+GustavoLOliveiraOt@users.noreply.github.com>
2025-02-27 15:20:30 -0300

    recomendação caso não tenha uma noticia base

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import uuid
-
 from fastapi import FastAPI
-from uuid import UUID
+from typing import  Optional
 import pandas as pd
 
 app = FastAPI()
@@ -12,18 +10,22 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/noticias/relacionadas/{id}")
-async def noticias_relacionadas(id:str):
-    resultado = obter_noticias_relacionadas(id)
-    return resultado
+@app.get("/noticias/")
+async def noticias(id: Optional[str] = None):
+    if (id is None):
+        return obter_noticias_recentes()
+    else:
+        return obter_noticias_relacionadas(id)
 
 def obter_noticias_relacionadas(id:str):
-    df = pd.read_csv("static/noticias_clusterizadas.csv")
+    df = obter_noticias()
 
     noticia_principal = df.loc[df["page"] == id]
     cluster = noticia_principal['cluster'].item()
 
     noticias_por_cluster = df.loc[(df["cluster"] == cluster) & (df["page"] != id)]
+
+    noticias_por_cluster = noticias_por_cluster.drop(columns=["cluster"])
 
     noticias_por_cluster["issued"] = pd.to_datetime(noticias_por_cluster["issued"])
     noticias_por_cluster = noticias_por_cluster.sort_values(by="issued", ascending=False)
@@ -32,3 +34,18 @@ def obter_noticias_relacionadas(id:str):
 
     return noticias_por_cluster.to_dict(orient="records")
 
+def obter_noticias_recentes():
+    df = obter_noticias()
+
+    df = df.drop(columns=["cluster"])
+
+    df["issued"] = pd.to_datetime(df["issued"])
+    df = df.sort_values(by="issued", ascending=False)
+
+    df = df.head(10)
+
+    return df.to_dict(orient="records")
+
+def obter_noticias():
+    df = pd.read_csv("static/noticias_clusterizadas.csv")
+    return df
